e4cce4bbff37440dc85b067e80e06ac38b921e32
jguzz <joseph.a.guzzardo@gmail.com>
2020-03-23 17:03:41 -0400

    Updated user/watchlist controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,49 @@
 class UsersController < ApplicationController
+
+  before_action :find_user, only: [:show, :edit, :update]
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.valid?
+      redirect_to @user
+    else 
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_path
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    @user.update(user_params)
+    if @user.valid?
+      redirect_to @user
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to edit_user_path
   end
 
   def destroy
+    find_user.destroy
+    redirect_to new_user_path
   end
 
   def index
+    @users = User.all
   end
 
-  def show
+  def show; end
+
+  private 
+
+  def user_params
+    params.require(:user).permit!
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,10 +1,26 @@
 class WatchlistsController < ApplicationController
   def new
+    @watchlist = Watchlist.new
   end
 
   def create
+    @watchlist = Watchlist.create(watchlist_params)
+    if @watchlist.valid?
+      redirect_to @watchlist
+    else 
+      flash[:errors] = @watchlist.errors.full_messages
+      redirect_to new_watchlist_path
+    end
   end
 
   def destroy
+    find_user.destroy
+    redirect_to new_watchlist_path
+  end
+
+  private
+
+  def watchlist_params
+    params.require(:watchlist).permit!
   end
 end
