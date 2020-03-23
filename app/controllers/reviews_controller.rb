3761dcaf5e9098e5dc7d8aa3bd817cf1caa1ed4e
jguzz <joseph.a.guzzardo@gmail.com>
2020-03-23 16:48:40 -0400

    edit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,28 +1,22 @@
 class ReviewsController < ApplicationController
-  before_action :find_post,only: [:show, :edit, :update]
-  def index @posts=Post.all
-  end
-  def show end
   def new
-  @post = Post.new()
   end
-  end
+
   def create
-  @post = Post.create(post_params)
-  if @post.valid? redirect_to @post
-  else
-  end end
-  def edit end
-  def update @post.update(params.require(:post).permit!) if @post.valid?
-  redirect_to @post else
-  flash[:errors]=@post.erors.full_messages redirect_to edit_post_path
-  end end
-  def delete Post.find(params[:id]).delete redirect_to new_posts_page
   end
-  private
-  def post_params params.require(:post).permit!
+
+  def edit
   end
-  def find_post
-  @post = Post.find(params[:id])
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
   end
 end
