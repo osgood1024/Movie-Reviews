58f2331921f5ee9c31e9b8a7383bc4de54ed205e
jguzz <joseph.a.guzzardo@gmail.com>
2020-03-24 13:47:09 -0400

    Created views for watchlist and fixed views for user

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
 	has_many :reviews
 	has_many :users, through: :reviews
-	has_many :wishlists
-	has_many :users, through: :wishlists
+	has_many :watchlists
+	has_many :users, through: :watchlists
 end
