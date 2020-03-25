465fa377734fd0edbd409507dc72769d1d038409
jguzz <joseph.a.guzzardo@gmail.com>
2020-03-25 14:37:15 -0400

    API succesful, env gem added, api-key ignored

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+#Destroys before seeding
 Movie.destroy_all
 Review.destroy_all
 User.destroy_all
 Watchlist.destroy_all
 
-
-
-#Making the request to the movie database 
-resp = RestClient.get("https://api.themoviedb.org/3/movie/popular/?api_key=#{API_KEY}&language=en-US&page=1")
-#parse 
-data = JSON.parse(resp)
-
-popular_movies_list = data["results"]
-
-
- 
-popular_movies_list.each do |movie|
-  movie.each do |key, val|
-    if key == "title"
+api_key = ENV['SECRET_KEY']
+#page number to retrive data from
+page = 1
+#Loops through max amount of pages
+while page <= 1
+  #Making the request to the movie database 
+  resp = RestClient.get("https://api.themoviedb.org/3/movie/popular/?api_key=#{api_key}&language=en-US&page=#{page}")
+  #parse 
+  data = JSON.parse(resp)
+  #setting the results to a variable 
+  popular_movies_list = data["results"]
+  #looping through the data to fill our movie database
+  popular_movies_list.each do |movie|
       Movie.create({
-        title: val
+        title: movie["title"]
       })
-    end
   end
+  #Increments to the next page
+  page += 1
 end
 
+#Creats 20 dummy users
+20.times do 
+  User.create({
+      name: Faker::Name.name_with_middle
+  })
+end
 
+#Creates 40 dummy reviews
+40.times do 
+  Review.create({
+    rating: Faker::Number.between(from: 1, to: 10),
+    description: Faker::String.random(length: 30..60),
+    likes: Faker::Number.between(from: 1, to: 30),
+    movie_id: Movie.all.sample.id,
+    user_id: User.all.sample.id
+  })
+end
 
-
+#Creates 30 dummy Watchlists
+30.times do 
+  Watchlist.create({
+      movie_id: Movie.all.sample.id,
+      user_id: User.all.sample.id
+  })
+end
