90c7c00875f7e09b574c757adb881189866bfce8
osgood gunawan <ogunawan@ucsd.edu>
2020-03-27 14:08:34 -0400

    search added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,24 @@
 class MoviesController < ApplicationController
   def index
-    @movies=Movie.all
+    # @movies=Movie.all
+    if params[:search]
+      @movies=Movie.where('title LIKE ?', "%#{params[:search]}%")
+    else
+      @movies=Movie.all
+    end
   end
 
   def show
     @movie=Movie.find(params[:id])
   end
+
+
+
+
+  private
+
+  # def movie_params
+  #   params.require(:movie).permit!
+  # end
+
 end
